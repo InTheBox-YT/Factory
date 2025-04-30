b6fb2fc7bd473cf8fcdc3b7bdfe926560aff92aa
Jaycob2464 <pedersenjaycob@gmail.com>
2025-05-01 09:54:37 +1200

    w

--- funky_unc_needs_to_spunk.gd
+++ funky_unc_needs_to_spunk.gd
@@ -1,14 +1,18 @@
 extends Node3D
 
-var scene = preload("res://sub_viewport_container.tscn").instantiate()
+var Player = preload("res://Scenes/Player.tscn").instantiate()
 #var cur_scene = preload("res://funky unc needs to spunk.tscn")
 #var tree = get_tree()
 
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 @onready var control = $CanvasLayer/Control
 
 func _on_button_pressed() -> void:
-	get_tree().root.add_child(scene)
-#	get_tree().root.remove_child(cur_scene)
 	control.hide()
+	
+	var instance = Player.instantiate()
+	add_child(instance)
+	
 	
