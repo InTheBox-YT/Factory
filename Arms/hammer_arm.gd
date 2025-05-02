1f40046361ed02062c6e245af33ac4e375fde115
InTheBox-YT <intheboxyt@gmail.com>
2025-05-02 16:25:08 +1200

    aa

--- Arms/hammer_arm.gd
+++ Arms/hammer_arm.gd
@@ -3,8 +3,8 @@ extends Node3D
 @onready var hammer = $Robotarm2
 
 var is_slamming = false
-var slam_time := 0.08  # Faster slam
-var return_time := 0.2  # Slower reset
+var slam_time := 0.08  
+var return_time := 0.2  
 var timer := 0.0
 var direction := 1  
 
@@ -12,7 +12,7 @@ var original_rotation := Vector3(deg_to_rad(0), deg_to_rad(90), deg_to_rad(-180)
 var target_rotation := Vector3(deg_to_rad(0), deg_to_rad(130), deg_to_rad(-272))
 
 var is_paused_for_impact := false
-var impact_pause_duration := 0.05  # Short freeze frame
+var impact_pause_duration := 0.05
 var impact_pause_timer := 0.0
 
 func _ready():
