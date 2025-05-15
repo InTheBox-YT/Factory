5fc0bd640ce9ac5fa96439e666c84eb5ad8bbed1
Jaycob2464 <pedersenjaycob@gmail.com>
2025-05-16 09:43:41 +1200

    I DID IT

--- Scripts/funky_unc_needs_to_spunk.gd
+++ Scripts/funky_unc_needs_to_spunk.gd
@@ -1,18 +1,19 @@
 extends Node3D
 
-var Player = preload("res://Scenes/Player.tscn").instantiate()
-#var cur_scene = preload("res://funky unc needs to spunk.tscn")
+var Player = preload("res://Scenes/sub_viewport_container.tscn")
 #var tree = get_tree()
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
-@onready var control = $CanvasLayer/Control
+@onready var control = $Node/CanvasLayer/Control
 
 func _on_button_pressed() -> void:
 	control.hide()
-	
 	var instance = Player.instantiate()
 	add_child(instance)
+	get_node("Node").queue_free()
+	
+	
 	
 	
