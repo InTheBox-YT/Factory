3344ee1332321bc0d2681f1035bf44308ea8f23f
Jaycob2464 <pedersenjaycob@gmail.com>
2025-05-16 14:07:00 +1200

    More Pause

--- Scenes/world.gd
+++ Scenes/world.gd
@@ -1,6 +1,8 @@
 extends Node3D
 @onready var pmenu = $PauseMenu
 var paused = false
+@onready var player = $Player
+@onready var playerscript = ("res://Scripts/player.gd")
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("Esc"):
@@ -12,10 +14,11 @@ func pauseMenu():
 		pmenu.hide()
 		Engine.time_scale = 1
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		player.show()
 	else:
 		pmenu.show()
 		Engine.time_scale = 0
-		
+		player.hide()
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
 	paused = !paused
