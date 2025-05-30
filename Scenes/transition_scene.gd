37e3162a9e4a7ca14799a12d31459168ba4d47cf
Jaycob2464 <pedersenjaycob@gmail.com>
2025-05-30 13:14:45 +1200

    Transition and Shaders
    Kinda big day today right now though I half made a transition scene that plays on main menu start but it only plays first half and if it plays full the game breaks so I need to find a way to properly call it. and I also added the shaders to the day1 and 2 scenes

--- Scenes/transition_scene.gd
+++ Scenes/transition_scene.gd
@@ -1,4 +1,21 @@
-extends Node3D
+extends CanvasLayer
 
-func _ready() -> void:
-	pass
+signal on_transition_finished
+
+@onready var color_rect: ColorRect = $ColorRect
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _ready():
+	color_rect.visible = false
+	animation_player.animation_finished.connect(_on_animation_finished)
+	
+func _on_animation_finished(anim_name):
+	if anim_name == "Transition":
+		on_transition_finished.emit()
+		animation_player.play("Fade_Out")
+	elif anim_name == "Fade_out":
+		color_rect.visible = false
+		
+func transition():
+	color_rect.visible = true
+	animation_player.play("Transition")
--- Scripts/funky_unc_needs_to_spunk.gd
+++ Scripts/funky_unc_needs_to_spunk.gd
@@ -1,12 +1,23 @@
 extends Node3D
 
+var TransitionScene = preload("res://Scenes/Transition Scene.tscn")
 var BaseScene = preload("res://Scenes/sub_viewport_container.tscn") #Preload the base scene
+
+var transition_node : Node = null
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE) #Hide the cursor
 
 func _on_button_pressed() -> void:
+	#Bring the Transition scene
+	if transition_node == null:
+		transition_node = TransitionScene.instantiate()
+		add_child(transition_node)
+	transition_node.transition()
+	await transition_node.on_transition_finished
+	transition_node.queue_free()
+	
 	var instance = BaseScene.instantiate()
-	add_child(instance)                    #Switch scenes and unload last scene on button press
+	add_child(instance)       #Switch scenes and unload last scene on button press
 	get_node("Node").queue_free()
 	print("Day 0 Loaded")
