9b2703ec76191cb97f0ab35d3c962f323fe17c10
Jaycob2464 <pedersenjaycob@gmail.com>
2025-07-18 13:35:16 +1200

    NOTHING
    Dammit antonio
    all your fault

---
+++ Scenes/ConveyorSound.gd
@@ -0,0 +1,10 @@
+extends Node3D
+
+@onready var audio_player: AudioStreamPlayer3D = $AudioPlayer2
+
+func _ready():
+	if audio_player.stream:
+		audio_player.play()
+	audio_player.connect("finished", _on_audio_finished)
+func _on_audio_finished():
+	audio_player.play()
--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -1,6 +1,14 @@
 extends Control
 
 
+func _ready() -> void:
+	pause_menu.hide()
+	
+@onready var pause_menu = $"."
 
 func _on_button_pressed() -> void:
 	get_tree().quit()
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Esc"):
+		pause_menu.show()
